69810b1d391e834e70020512cea2a6aec004ce7a
Hamed <hamed.vaheb.001@student.uni.lu>
2022-10-25 16:51:15 +0200

    blablalba

--- my_script.R
+++ my_script.R
@@ -12,3 +12,8 @@ lm_mod <- lm(mtcars$cyl ~ mtcars$mpg)
 lm_mod$coefficients
 
 data(iris)
+
+https://gist.github.com/b-rodrigues/1e66297254e51ec409dc98376cbb009e
+
+Add all files that got modified:
+git add .
